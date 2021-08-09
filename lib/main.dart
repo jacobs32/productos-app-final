e2e372650bdc93cee0256a820e01a3523b417b11
Jacob Cardenas <201520407e@utea.edu.pe>
2021-08-09 11:09:25 -0500

    Agregando HomeScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:productos_app/screens/screens.dart';
- 
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -11,12 +11,16 @@ class MyApp extends StatelessWidget {
       title: 'Productos App',
       initialRoute: 'home',
       routes: {
-        'login': ( _ ) => LoginScreen(),
-        'home' : ( _ ) => HomeScreen(),
+        'login': (_) => LoginScreen(),
+        'home': (_) => HomeScreen(),
       },
       theme: ThemeData.light().copyWith(
-        scaffoldBackgroundColor: Colors.grey[300]
-      ),
+          scaffoldBackgroundColor: Colors.grey[300],
+          appBarTheme: AppBarTheme(elevation: 0, color: Colors.indigo),
+          floatingActionButtonTheme: FloatingActionButtonThemeData(
+            backgroundColor: Colors.indigo,
+            elevation: 0,
+          )),
     );
   }
 }
