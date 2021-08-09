b3ed6059c7327876b4c3b2151ebe5871b569dc23
Jacob Cardenas <201520407e@utea.edu.pe>
2021-08-09 11:10:53 -0500

    "Pantalla de productos - HomeScreen"

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
-
+import 'package:productos_app/widgets/product_card.dart';
 
 class HomeScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('HomeScreen'),
-     ),
-   );
+      appBar: AppBar(
+        title: Text('Productos'),
+      ),
+      body: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (BuildContext context, int index) => ProductCard()),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+    );
   }
 }
